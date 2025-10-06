53167e713edace87d0eb8511c66a850756777af3
polotuspossumus-coder <polotuspossumus@gmail.com>
2025-10-06 15:19:23 -0500

    Initial legacy drop

---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# Fortheweebs Runtime
+
+## Build & Package
+
+```bash
+npm install
+npm run build
+npm run package
---
+++ electron.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    icon: path.join(__dirname, 'assets/icon.png'), // Optional: add app icon
+    show: false // Don't show until ready
+  });
+
+  win.loadFile('dist/index.html');
+  
+  // Show window when ready to prevent visual flash
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
+  // Open DevTools in development
+  if (process.env.NODE_ENV === 'development') {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
