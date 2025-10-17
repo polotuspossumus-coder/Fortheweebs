dc850cbf706c4d3a74c4b0ece0b5bbac668b90ec
polotuspossumus-coder <polotuspossumus@gmail.com>
2025-10-16 23:13:13 -0500

    Add /api/remix-chain endpoint for remix chain display

---
+++ api/remix-chain.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./lib/database');
+
+router.get('/:slabId', async (req, res) => {
+  try {
+    const chain = [];
+    let current = await db.slabs.findOne({ _id: req.params.slabId });
+    while (current) {
+      chain.unshift(current);
+      current = current.remixOf ? await db.slabs.findOne({ _id: current.remixOf }) : null;
+    }
+    res.json(chain.map(s => ({ id: s._id, name: s.name })));
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch remix chain.' });
+  }
+});
+
+module.exports = router;
