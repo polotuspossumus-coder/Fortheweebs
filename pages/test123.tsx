e63c31a2fde3560455cf1de83283a6931a9dcc8f
polotuspossumus-coder <polotuspossumus@gmail.com>
2025-10-18 00:40:42 -0500

    Add test123 page to verify Next.js deployment

---
+++ pages/test123.tsx
@@ -0,0 +1,7 @@
+export default function Test123() {
+  return (
+    <main style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#222', color: '#fff' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>This is the test123 page. If you see this, Next.js is working!</h1>
+    </main>
+  );
+}
