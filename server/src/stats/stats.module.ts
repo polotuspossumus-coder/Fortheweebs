322c26cfaa0b887d16a7f3d6d8e2755471cd89a4
polotuspossumus-coder <your@email.com>
2025-11-18 02:00:16 -0600

    Fix StatsModule - import JwtModule for JwtAuthGuard

--- server/src/stats/stats.module.ts
+++ server/src/stats/stats.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { StatsService } from './stats.service';
 import { StatsController } from './stats.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({}),
+  ],
   controllers: [StatsController],
   providers: [StatsService],
   exports: [StatsService],
